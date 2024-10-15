68b011c636362d237765a64463f0e24ecf3f4c5d
JMAC137 <51169542+JonMariC137@users.noreply.github.com>
2024-10-16 00:08:32 +0800

    Battle.java and Protagonist modification
    Nagbutang ko ug Battle interface para sa Protagonist ug sa Enemy(inig ugma pa)

---
+++ src/echoes/of/the/dead/Battle.java
@@ -0,0 +1,10 @@
+package echoes.of.the.dead;
+
+
+
+// Used to interact with other "Entity/s"
+public interface Battle{
+    public void skill1();
+    public void skill2();
+    public void skill3();
+}
