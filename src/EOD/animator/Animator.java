fc7c81665dc5b8606a06c919f0742ff061f04a11
joanaxyz <joanacarlagako15@gmail.com>
2024-10-23 12:54:54 +0700

    removed debugger for skill frames

--- src/EOD/animator/Animator.java
+++ src/EOD/animator/Animator.java
@@ -99,7 +99,7 @@ public abstract class Animator {
             currentFrame = Math.min(currentFrame + 1, deadSprites.getSize() - 1);
         } else if (isUsingSkill) {
             updateSkillAnimation();
-            System.out.println("Character: " + character.getName() + "skill frame: " + skillAnimationFrame + "is moving: " + isMoving);
+            // System.out.println("Character: " + character.getName() + "skill frame: " + skillAnimationFrame + "is moving: " + isMoving);
         } else {
             ImageList currentSprites = isMoving ? walkSprites : idleSprites;
             currentFrame = (currentFrame + 1) % currentSprites.getSize();
