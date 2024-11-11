b97374f18ecd216ff44e9cc52f467a7fc64d9d23
YelzyDy <zendymariel.dy@gmail.com>
2024-11-11 22:53:06 +0800

    button names

--- src/EOD/worlds/World.java
+++ src/EOD/worlds/World.java
@@ -187,9 +187,9 @@ public abstract class World extends javax.swing.JFrame implements MouseInteracta
     }
 
     public void initializeChoiceUI() {
-        choicepan = new ChoiceUI();
+        choicepan = new ChoiceUI(player);
         // Set ChoiceUI position and size: yOffset is the height of the image (40% of the screen)
-        choicepan.setBounds(0, (int) (screenSize.height * 0.4), (int) (screenSize.width * 0.99), (int) (screenSize.height * 0.6));
+        choicepan.setBounds(0, (int) (screenSize.height * 0.4), (int) (screenSize.width), (int) (screenSize.height * 0.6));
         layeredPane.add(choicepan, Integer.valueOf(2));
         layeredPane.revalidate();
         layeredPane.repaint();
