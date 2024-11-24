740b7887a9d623d1274356d3f132067db427152c
JMAC137 <51169542+JonMariC137@users.noreply.github.com>
2024-11-24 21:38:02 +0800

    Update SceneBuilder.java

--- src/EOD/scenes/SceneBuilder.java
+++ src/EOD/scenes/SceneBuilder.java
@@ -382,9 +382,9 @@ public void createWorldScene() {
                 if(obj.getName().equals("portalNextWorld") && currentSceneIndex == 2)obj.setVisible(true);
                 
                    // comment out the code above if ganahan mu nextWorldPortal kay naa dayun pero sa last scene ra sha ma click
-                // if(obj.getName().equals("portalMiniBoss")){
-                //     obj.setVisible(true);
-                // }
+                if(obj.getName().equals("portalMiniBoss")){
+                    obj.setVisible(true);
+                }
             }
             else{
                 obj.setVisible(obj.getIndex() == currentSceneIndex);
