f73ee70e5727e796225af83daa43b9d1e6cdbb73
YelzyDy <zendymariel.dy@gmail.com>
2024-11-24 22:57:22 +0800

    gi show ra miniboss portal

--- src/EOD/scenes/SceneBuilder.java
+++ src/EOD/scenes/SceneBuilder.java
@@ -380,6 +380,7 @@ public void createWorldScene() {
             if(obj.getName().equals("portal") || obj.getName().equals("portalMiniBoss") || obj.getName().equals("portalNextWorld")){
                 obj.setVisible(obj.getIndex() == currentSceneIndex && obj.getIsActivated()); 
                 if(obj.getName().equals("portalNextWorld") && currentSceneIndex == 2)obj.setVisible(true);
+                if(obj.getName().equals("portalMiniBoss") && currentSceneIndex == 2)obj.setVisible(true);
                 
                    // comment out the code above if ganahan mu nextWorldPortal kay naa dayun pero sa last scene ra sha ma click
                  //if(obj.getName().equals("portalMiniBoss")){
